7ccc34afa8ffc184617043f18ae046e9ff82a82f
Roman Birukov <r.v.birukov@yandex.ru>
2017-06-30 16:54:36 +0300

    init

--- Obmen/Operation.cs
+++ Obmen/Operation.cs
@@ -194,9 +194,23 @@ namespace Obmen
             CopyFtp.Password = pass;
 
             CopyFtp.Copy(pensiaFrom, pensiaTo);
-            CopyFtp.Copy(f130From, f130To);
-            CopyFtp.Copy(regPostPayFrom, regPostPayTo);
-            CopyFtp.Copy(regFSGFrom, regFSGTo);
+            //CopyFtp.Copy(f130From, f130To);
+            //CopyFtp.Copy(regPostPayFrom, regPostPayTo);
+            //CopyFtp.Copy(regFSGFrom, regFSGTo);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Operation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(334, 132);
+            this.Name = "Operation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
     }
 }
