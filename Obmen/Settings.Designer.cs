eb54c6a590ebde52e3c9261a02cd6b44eeeb2211
Roman Birukov <r.v.birukov@yandex.ru>
2017-06-30 09:53:31 +0300

    new

--- Obmen/Settings.Designer.cs
+++ Obmen/Settings.Designer.cs
@@ -428,7 +428,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(72, 9);
+            this.label2.Location = new System.Drawing.Point(56, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(360, 20);
             this.label2.TabIndex = 59;
